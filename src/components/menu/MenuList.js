af1070015ceeb9008bb17b584c0a9d7b67f69e4c
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-07-19 19:11:51 +0330

    update

--- src/components/menu/MenuList.js
+++ src/components/menu/MenuList.js
@@ -3,9 +3,9 @@ import MenuItem from './MenuItem';
 
 const MenuList = ({ menuItems }) => {
   return (
-    <Grid container spacing={1.5}>
+    <Grid container spacing={1} sx={{ padding: '8px' }}>
       {menuItems.map((item) => (
-        <Grid item xs={12} sm={6} key={item.id}>
+        <Grid item xs={4} key={item.id}> {/* تغییر به 3 ستون */}
           <MenuItem item={item} />
         </Grid>
       ))}
